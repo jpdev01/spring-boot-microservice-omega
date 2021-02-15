705b21aff525f7790949eb6bed28a0e0951bc950
João Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-14 21:35:33 -0300

    Construção das entidades de pagamento e venda

--- src/main/java/onroad/entity/Payment.java
+++ src/main/java/onroad/entity/Payment.java
@@ -1,4 +1,21 @@
 package onroad.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
 public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Float value;
+    private Date date;
+    private String description;
+    private Sale sale;
+    private String frmPgto;
+    private String method;
 }
--- src/main/java/onroad/entity/Sale.java
+++ src/main/java/onroad/entity/Sale.java
@@ -1,2 +1,22 @@
-package onroad.entity;public class Sale {
+package onroad.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Float totalValue;
+    private Date date;
+    private String description;
+    private String method;
+
+    public Sale() {
+    }
 }
