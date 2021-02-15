f6e8badfc5acedc47c15808fd176bf6f5735d1de
João Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-14 21:26:31 -0300

    Criação das entidades para controle de caixa e endereços

--- src/main/java/onroad/entity/Address.java
+++ src/main/java/onroad/entity/Address.java
@@ -1,2 +1,20 @@
-package onroad.entity;public class Address {
+package onroad.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String cep;
+    private String uf;
+    private String publicPlace;
+    private String neighborhood;
+    private String city;
+    private String complement;
 }
--- src/main/java/onroad/entity/Cashier.java
+++ src/main/java/onroad/entity/Cashier.java
@@ -1,2 +1,18 @@
-package onroad.entity;public class Cashier {
+package onroad.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Cashier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer actualValue;
+    private Date closeDate;
+    private String description;
 }
