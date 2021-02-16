220e0f5c0037c4ce24aafc25a874e10245e12cb1
João Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-15 22:15:35 -0300

    A anotação fará que o enum vire tipo int no banco

--- src/main/java/onroad/dao/CashierDAO.java
+++ src/main/java/onroad/dao/CashierDAO.java
@@ -1,6 +1,13 @@
 package onroad.dao;
 
+import onroad.entity.Cashier;
+
+import java.util.Set;
+
 public interface CashierDAO {
 
-    public void save(CashierDAO cashier);
+    public void save(Cashier cashier);
+    public Cashier findById(Integer id);
+    public Set<Cashier> findAll();
+    public void remove(Integer id);
 }
