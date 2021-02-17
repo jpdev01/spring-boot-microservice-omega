cef2cb02a7d333ddfcd76b79d8a0b02e9dc9eb8c
João Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-16 22:56:50 -0300

    Classe para simular Cashier

--- src/main/java/onroad/simulation/CachierTest.java
+++ src/main/java/onroad/simulation/CachierTest.java
@@ -1,2 +1,15 @@
-package onroad.simulation;public class CachierTest {
+package onroad.simulation;
+
+import onroad.entity.Cashier;
+
+public class CachierTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void saveTest()
+    {
+        Cashier cashier = new Cashier();
+    }
 }
