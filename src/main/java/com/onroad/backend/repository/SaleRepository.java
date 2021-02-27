38e05ff53e5704a2fb865a91a15d1cd87f671d60
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-27 02:11:38 -0300

    implementando anotação @Repository faltante

--- src/main/java/com/onroad/backend/repository/SaleRepository.java
+++ src/main/java/com/onroad/backend/repository/SaleRepository.java
@@ -2,6 +2,8 @@ package com.onroad.backend.repository;
 
 import com.onroad.backend.entity.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SaleRepository extends JpaRepository<Sale, Integer> {
 }
---
+++ src/main/java/com/onroad/backend/service/SaleService.java
@@ -0,0 +1,16 @@
+package com.onroad.backend.service;
+
+import com.onroad.backend.repository.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+public class SaleService {
+
+    @Autowired
+    private SaleRepository repository;
+
+    public void save(Repository repository)
+    {
+
+    }
+}
