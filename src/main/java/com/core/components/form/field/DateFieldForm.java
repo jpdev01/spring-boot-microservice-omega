d15740d0da80da05a882ed1c234a80bda7eca9f2
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-05-04 21:32:27 -0300

    date field form

---
+++ src/main/java/com/core/components/form/field/DateFieldForm.java
@@ -0,0 +1,9 @@
+package com.core.components.form.field;
+
+public class DateFieldForm extends FieldForm{
+
+    public DateFieldForm(String id, String label, String group)
+    {
+        super(id, label, group, FieldFormType.DATE);
+    }
+}
