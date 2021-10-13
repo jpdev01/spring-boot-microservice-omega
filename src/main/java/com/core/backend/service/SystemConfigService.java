892acc7b7b32769fbc9fb2036ada5181fb219e9f
Joao Pedro Truchinski Borba <joao.borba@neomind.com.br>
2021-10-12 22:36:05 -0300

    system config service

--- src/main/java/com/core/backend/service/SystemConfigService.java
+++ src/main/java/com/core/backend/service/SystemConfigService.java
@@ -5,6 +5,7 @@ import com.core.backend.repository.SystemConfigRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
+@Service
 public class SystemConfigService
 {
 	@Autowired
