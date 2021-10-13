214a06c37e2733b944056673a1615acdc8e1098b
Joao Pedro Truchinski Borba <joao.borba@neomind.com.br>
2021-10-12 22:26:57 -0300

    indent

--- src/main/java/com/core/backend/CorsConfiguration.java
+++ src/main/java/com/core/backend/CorsConfiguration.java
@@ -7,9 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class CorsConfiguration implements WebMvcConfigurer {
+public class CorsConfiguration implements WebMvcConfigurer
+{
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedOrigins("Access-Control-Allow-Origin").allowedHeaders("Content-type", "Authorization");
+    public void addCorsMappings(CorsRegistry registry)
+    {
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("Access-Control-Allow-Origin")
+            .allowedHeaders("Content-type", "Authorization");
     }
 }
